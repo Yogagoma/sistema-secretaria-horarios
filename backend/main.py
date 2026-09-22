67add8b54e5466cb596ba6fecf61bb6e3c3867be
unknown <yosgluisdev@gmail.com>
2026-09-22 14:45:51 -0400

    feat[backend]: añadir middleware de CORS para permitir peticiones del frontend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models.base import Base
 
 Base.metadata.create_all()
 
 # Inicializar servidor fastAPI
 app = FastAPI()
+
+# Agregar el middleware del sistema
+app.add_middleware(
+    CORSMiddleware, # Habilita el Intercambio de Recursos de Origen Cruzado(CORS)
+    allow_origins=["http://localhost:8000"], # Especifica la ruta y el puerto 8000
+    allow_methods= ["*"] # Permite todos los métodos del backend
+)
