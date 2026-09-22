8aa80784209c615c5b3b2dffde900eaa93cc518e
unknown <yosgluisdev@gmail.com>
2026-09-22 17:02:13 -0400

    fix [backend]: indicar puertos de conexiones entre frontend y backend

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from database import engine
 from models.base import Base
 
-Base.metadata.create_all()
+Base.metadata.create_all(bind=engine)
 
 # Inicializar servidor fastAPI
 app = FastAPI()
 
+origenes = [
+    "http://localhost:3000", # Puerto del frontend
+    "http://localhost:8000" # Puerto del backend
+]
+
 # Agregar el middleware del sistema
 app.add_middleware(
     CORSMiddleware, # Habilita el Intercambio de Recursos de Origen Cruzado(CORS)
-    allow_origins=["http://localhost:8000"], # Especifica la ruta y el puerto 8000
+    allow_origins=origenes, # Especifica la ruta los puertos
     allow_methods= ["*"] # Permite todos los métodos del backend
 )
